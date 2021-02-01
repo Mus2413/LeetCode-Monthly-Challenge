5dca2557d3ebe40623c83c8a83ec9cf5cf069cfe
Muskan Jain <56889261+Mus2413@users.noreply.github.com>
2021-02-01 15:40:26 +0530

    Add files via upload

---
+++ February_leetcode_challenge/Number of 1bit.cpp
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+    int ans = 0;
+	while (n)
+	{
+		ans += n % 2;
+		n >>= 1;
+	}
+	return ans;
+	
+        
+    }
+};
