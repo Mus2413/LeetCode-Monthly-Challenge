8554c10d095f6ec59bb8a4ab4d72f6c34cb7b3f2
Muskan Jain <56889261+Mus2413@users.noreply.github.com>
2021-05-09 21:57:46 +0530

    Add files via upload

---
+++ May Leetcode Challenge/check target array is possible.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& target) {
+        if(target.size()==1&&target[0]==1)
+            return true;
+        if(target.size()==1 &&target[0]>1)
+            return false;
+        priority_queue<long long int> pq;
+        long long int sum=0;
+        bool flag=true;
+        for(int i=0;i<target.size();i++)
+        {
+            pq.push(target[i]);
+            sum+=target[i];
+        }
+        while(pq.top()!=1)
+        {
+            long long int num = pq.top();
+            pq.pop();
+            long long int new_sum = sum - num; 
+            num =num-(sum-num);// Tracing back to original sum
+            
+            if(num<1)
+            {
+                flag = false;
+                break;
+            }
+            num=num%(sum-num);// If it is still the max number then we don't want to do same operation multiple times.
+            if(num==0)
+            {
+                flag = false;
+                break;
+            }
+            pq.push(num);
+            sum = new_sum + num;
+        }
+        return flag;
+    
+        
+    }
+};
